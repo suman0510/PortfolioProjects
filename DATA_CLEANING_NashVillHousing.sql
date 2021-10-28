a41de23ce1748fa9d4fc83611740227487949618
suman0510 <84570149+suman0510@users.noreply.github.com>
2021-10-28 09:17:31 +0530

    Update DATA_CLEANING_NashVillHousing.sql

--- DATA_CLEANING_NashVillHousing.sql
+++ DATA_CLEANING_NashVillHousing.sql
@@ -19,44 +19,44 @@ ALTER COLUMN SaleDate DATE--executed Succesfully
 
 SELECT *
 FROM dataCleaning_portfolio..NashVillHousing
-where PropertyAddress is NULL ---rows where the propertyAddress isnull
+WHERE PropertyAddress IS NULL ---rows where the propertyAddress isnull
 
 
 SELECT a.ParcelID,a.PropertyAddress,b.ParcelID,b.PropertyAddress,ISNULL(a.PropertyAddress,b.PropertyAddress)
-from dataCleaning_portfolio..NashVillHousing as a
-join dataCleaning_portfolio..NashVillHousing as b
-on a.ParcelID=b.ParcelID
+FROM dataCleaning_portfolio..NashVillHousing AS a
+JOIN dataCleaning_portfolio..NashVillHousing AS b
+ON a.ParcelID=b.ParcelID
 AND a.[UniqueID ]<>B.[UniqueID ]
-WHERE a.PropertyAddress is null
+WHERE a.PropertyAddress IS null
 
 UPDATE a
-set PropertyAddress=ISNULL(a.PropertyAddress,b.PropertyAddress)
-from dataCleaning_portfolio..NashVillHousing as a
-join dataCleaning_portfolio..NashVillHousing as b
-on a.ParcelID=b.ParcelID
+SET PropertyAddress=ISNULL(a.PropertyAddress,b.PropertyAddress)
+FROM dataCleaning_portfolio..NashVillHousing AS a
+JOIN dataCleaning_portfolio..NashVillHousing AS b
+ON a.ParcelID=b.ParcelID
 AND a.[UniqueID ]<>B.[UniqueID ]
-WHERE a.PropertyAddress is null
+WHERE a.PropertyAddress IS null
 
 
 --------breaking address
 SELECT 
-SUBSTRING(PropertyAddress,1,CHARINDEX(',',PropertyAddress)-1) as AddressStreet,
+SUBSTRING(PropertyAddress,1,CHARINDEX(',',PropertyAddress)-1) AS AddressStreet,
 SUBSTRING(PropertyAddress,CHARINDEX(',',PropertyAddress)+1,LEN(PropertyAddress)) AS AddressCity
-from dataCleaning_portfolio..NashVillHousing
+FROM dataCleaning_portfolio..NashVillHousing
 
 
 ALTER TABLE dataCleaning_portfolio..NashVillHousing
-ADD PropertyAddressStreet nvarchar(40)
+ADD PropertyAddressStreet NVARCHAR(40)
 
-update dataCleaning_portfolio..NashVillHousing
-set PropertyAddressStreet=SUBSTRING(PropertyAddress,1,CHARINDEX(',',PropertyAddress)-1)
+UPDATE dataCleaning_portfolio..NashVillHousing
+SET PropertyAddressStreet=SUBSTRING(PropertyAddress,1,CHARINDEX(',',PropertyAddress)-1)
 
 
 ALTER TABLE dataCleaning_portfolio..NashVillHousing
-ADD PropertyAddressCity nvarchar(25)
+ADD PropertyAddressCity NVARCHAR(30)
 
-update  dataCleaning_portfolio..NashVillHousing
-set PropertyAddressCity=SUBSTRING(PropertyAddress,CHARINDEX(',',PropertyAddress)+1,LEN(PropertyAddress))
+UPDATE  dataCleaning_portfolio..NashVillHousing
+SET PropertyAddressCity=SUBSTRING(PropertyAddress,CHARINDEX(',',PropertyAddress)+1,LEN(PropertyAddress))
 
 
 --delete the previous one(propretyAddress)
@@ -67,18 +67,18 @@ DROP COLUMN PropertyAddress
 -------Owner's Addres
 Select *
 FROM  dataCleaning_portfolio..NashVillHousing
-where OwnerAddress is null;
+where OwnerAddress IS NULL;
 
 SELECT PARSENAME(REPLACE(OwnerAddress,',','.'),3),
        PARSENAME(REPLACE(OwnerAddress,',','.'),2),
 	   PARSENAME(REPLACE(OwnerAddress,',','.'),1)
-from  dataCleaning_portfolio..NashVillHousing
+FROM  dataCleaning_portfolio..NashVillHousing
 
-ALTER table dataCleaning_portfolio..NashVillHousing
-add OwnerAddressStreet nvarchar(30),OwnerAddressCity nvarchar(30),OwnerAddressState nvarchar(30)
+ALTER TABLE dataCleaning_portfolio..NashVillHousing
+ADD OwnerAddressStreet nvarchar(30),OwnerAddressCity nvarchar(30),OwnerAddressState nvarchar(30)
 
-update  dataCleaning_portfolio..NashVillHousing
-set OwnerAddressStreet=PARSENAME(REPLACE(OwnerAddress,',','.'),3),
+UPDATE  dataCleaning_portfolio..NashVillHousing
+SET OwnerAddressStreet=PARSENAME(REPLACE(OwnerAddress,',','.'),3),
     OwnerAddressCity=PARSENAME(REPLACE(OwnerAddress,',','.'),2),
 	OwnerAddressState= PARSENAME(REPLACE(OwnerAddress,',','.'),1)
 
@@ -95,32 +95,32 @@ SET  SoldAsVacant= REPLACE(SoldAsVacant, 'N', 'NO')--Ithought this would work
                                                   --But,it replaced every N and Y like..no necame noo ad yes became Yess
 												  --so go for the next method--use this only when the thing replacing id unique
 --another way
-Select SoldAsVacant,Case when SoldAsVacant='Yess' Then 'Yes'
-                         when SoldAsVacant='NOo' then 'No' 
-						 else SoldAsVacant
-						 End
-from dataCleaning_portfolio..NashVillHousing
+SELECT SoldAsVacant,CASE WHEN SoldAsVacant='Yess' THEN 'Yes'
+                         WHEN SoldAsVacant='NOo' THEN 'No' 
+						 ELSE SoldAsVacant
+						 END
+FROM dataCleaning_portfolio..NashVillHousing
 
-Update  dataCleaning_portfolio..NashVillHousing
-set SoldAsVacant=Case when SoldAsVacant='Yess' Then 'Yes'
-                         when SoldAsVacant='NOo' then 'No' 
-						 else SoldAsVacant
-						 End
+UPDATE  dataCleaning_portfolio..NashVillHousing
+SET SoldAsVacant=CASE WHEN SoldAsVacant='Yess' THEN 'Yes'
+                         WHEN SoldAsVacant='NOo' THEN 'No' 
+						 ELSE SoldAsVacant
+						 END
 
 ----------Remove Duplicates-------------------------------------
 -----lets do it with CTE's
-with ROWNUM as(
+WITH ROWNUM AS(
 SELECT *, 
-  ROW_NUMBER() Over (
-  Partition by ParcelID,PropertyAddressStreet,PropertyAddressCity,SalePrice,SaleDate,LegalReference
-  Order by UniqueID) row_num
+  ROW_NUMBER() OVER (
+  PARTITION BY ParcelID,PropertyAddressStreet,PropertyAddressCity,SalePrice,SaleDate,LegalReference
+  ORDER BY UniqueID) row_num
 FROM dataCleaning_portfolio..NashVillHousing)
 
 Delete
-from ROWNUM
-where row_num>1
+FROM ROWNUM
+WHERE row_num>1
 
 
 --Delete UnWanted Columns----I found only the taxDistrict to be not needed.
-Alter table  dataCleaning_portfolio..NashVillHousing
+ALTER TABLE  dataCleaning_portfolio..NashVillHousing
 DROP column TaxDistrict
